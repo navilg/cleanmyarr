6b182403c3f4ed01f532728e3f0e5b906d8345d2
Navratan Lal Gupta <navilg0409@gmail.com>
2023-08-27 23:25:39 +0530

    enable argument input for smtp

--- internal/initialize.go
+++ internal/initialize.go
@@ -4,12 +4,18 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 // var sample_config_file_url string = "https://raw.githubusercontent.com/navilg/cleanmyarr/main/sample-config.yaml"
 var sampleConfigFile string = "sample-config.yaml"
 
-func Initialize(configFile string) error {
+func InitializeConfig(configFile string) error {
+
+	var err error = nil
 
 	log.Println("Initializing...")
 
@@ -28,6 +34,97 @@ func Initialize(configFile string) error {
 			log.Println("Failed to initialize.")
 			return err
 		}
+	}
+
+	_, err = ReadConfig(configFile)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	maintenanceCycle := os.Getenv("CMA_MAINTENANCE_CYCLE")
+	if maintenanceCycle != "" {
+		Config.MaintenanceCycle = Interval(maintenanceCycle)
+	}
+
+	deleteAfterDays := os.Getenv("CMA_DELETE_AFTER_DAYS")
+	if deleteAfterDays != "" {
+		Config.DeleteAfterDays, err = strconv.Atoi(deleteAfterDays)
+		if err != nil {
+			return err
+		}
+	}
+
+	ignoreTag := os.Getenv("CMA_IGNORE_TAG")
+	if ignoreTag != "" {
+		Config.IgnoreTag = ignoreTag
+	}
+
+	smtpEnabled := os.Getenv("CMA_ENABLE_EMAIL_NOTIFICATION")
+	if smtpEnabled != "" {
+		Config.NotificationChannel.SMTP.Enabled, err = strconv.ParseBool(smtpEnabled)
+		if err != nil {
+			return err
+		}
+	}
+
+	smtpServer := os.Getenv("CMA_SMTP_SERVER")
+	if smtpServer != "" {
+		Config.NotificationChannel.SMTP.Server = smtpServer
+	}
+
+	smtpPort := os.Getenv("CMA_SMTP_PORT")
+	if smtpPort != "" {
+		Config.NotificationChannel.SMTP.Port, err = strconv.Atoi(smtpPort)
+		if err != nil {
+			return err
+		}
+	}
+
+	smtpSecurity := os.Getenv("CMA_SMTP_SECURITY")
+	if smtpSecurity != "" {
+		Config.NotificationChannel.SMTP.Security = Security(smtpSecurity)
+	}
+
+	smtpUsername := os.Getenv("CMS_SMTP_USERNAME")
+	if smtpUsername != "" {
+		Config.NotificationChannel.SMTP.Username = smtpUsername
+	}
+
+	smtpb64Pass := os.Getenv("CMA_SMTP_ENCODED_PASSWORD")
+	if smtpb64Pass != "" {
+		Config.NotificationChannel.SMTP.B64Password = smtpb64Pass
+	}
+
+	smtpFromEmail := os.Getenv("CMA_SMTP_FROM_EMAIL")
+	if smtpFromEmail != "" {
+		Config.NotificationChannel.SMTP.FromEmail = smtpFromEmail
+	}
+
+	smtpToEmails := os.Getenv("CMA_SMTP_TO_EMAILS")
+	if smtpToEmails != "" {
+		Config.NotificationChannel.SMTP.ToEmail = strings.Split(smtpToEmails, ",")
+	}
+
+	ccEmails := os.Getenv("CMA_SMTP_CC_EMAILS")
+	if ccEmails != "" {
+		Config.NotificationChannel.SMTP.CcEmail = strings.Split(ccEmails, ",")
+	}
+
+	bccEmails := os.Getenv("CMA_SMTP_BCC_EMAILS")
+	if bccEmails != "" {
+		Config.NotificationChannel.SMTP.BccEmail = strings.Split(bccEmails, ",")
+	}
+
+	argConfigData, err := yaml.Marshal(Config)
+	if err != nil {
+		log.Println("Failed to initialize configuration file", err.Error())
+		return err
+	}
+
+	err = ioutil.WriteFile(configFile, argConfigData, 0644)
+	if err != nil {
+		log.Println("Failed to initialize configuration file", err.Error())
+		return err
 	}
 
 	log.Println("Initialization completed.")
